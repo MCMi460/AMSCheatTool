5d3cca882b98bebb936045a0ca98f125a8c42601
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2023-05-03 16:58:02 -0500

    Update building

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,4 +1,4 @@
 cd ..\
 python -m pip install -r AMScheat/requirements.txt pyinstaller
-python -m PyInstaller --onefile --clean --noconsole app.py
+python -m PyInstaller --onefile --clean --noconsole --name=AMSCheatTool --icon=resources/icon.ico app.py
 start dist
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 cd ../
 python3 -m pip install -r AMScheat/requirements.txt py2app
-py2applet --make-setup app.py
+py2applet --make-setup app.py resources/icon.icns
+sed -i '' -e "s/)/    name='AMSCheatTool')/" setup.py
 python3 setup.py py2app
 open dist
